a0b3c7c6a54e611dc4f745b2ef08cae3c3b56c83
“sriraam763” <your_email@gmail.com>
2026-03-20 11:34:15 +0530

    fix apiurl

--- web6/src/app/services.ts
+++ web6/src/app/services.ts
@@ -8,7 +8,7 @@ import { todo } from './todolist';
 })
 export class Services {
   private http=inject(HttpClient);
-  private apiurl="webappazure6-amgkc6egdzh3e0a8.centralindia-01.azurewebsites.net";
+  private apiurl="https://webappazure6-amgkc6egdzh3e0a8.centralindia-01.azurewebsites.net";
 
   gettask():Observable<todo[]>{
     return this.http.get<todo[]>(this.apiurl);
